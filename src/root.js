2c0c32a073478268a55ba6b0ceea5ba542809cc8
ShinaBR2 <ShinaBR2@gmail.com>
2020-06-28 18:32:25 +0700

    First add Firebase Analytics

--- src/root.js
+++ src/root.js
@@ -20,11 +20,13 @@ class RootElement extends React.Component {
   componentDidMount() {
     const app = import('firebase/app');
     const auth = import('firebase/auth');
+    const analytics = import('firebase/analytics');
 
-    Promise.all([app, auth]).then((values) => {
+    Promise.all([app, auth, analytics]).then((values) => {
       const firebase = getFirebase(values[0]);
       this.setState({ firebase });
 
+      firebase.analytics();
       firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
           this.setState({ user: null });
@@ -43,6 +45,11 @@ class RootElement extends React.Component {
 
   render = () => {
     const { firebase, user } = this.state;
+    if (!firebase) {
+      // For sure all thing need to be render only on client side
+      return null;
+    }
+
     return (
       <FirebaseContext.Provider value={{ firebase, user }}>
         <ApolloProvider client={apolloClient}>
